4c9268750fac273f92e812546cfb896e26cbcbc8
shomil03 <shomil02020@gmail.com>
2024-11-20 01:50:52 +0530

    added supports for concurrent clients

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,13 +6,19 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Main {
+public class Main extends Thread{
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
+    Main thread = new Main();
+    thread.run();
     //  Uncomment this block to pass the first stage
-       ServerSocket serverSocket = null;
+       
+  }
+
+  public void run(){
+    ServerSocket serverSocket = null;
        Socket clientSocket = null;
        int port = 6379;
        try {
