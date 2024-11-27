19a5031afde2c571fcbd265fec5a0649f136edc4
shomil03 <shomil02020@gmail.com>
2024-11-27 22:40:04 +0530

    bug fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -165,7 +165,7 @@ public class Main{
           case "psync":
                 response = makeBulkString("+FULLRESYNC "+master_replicationID+" "+master_replicationOffset+"\r\n", false);
                 clientSocket.getOutputStream().write(response.getBytes());
-                String rdbHexFile = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2";
+                String rdbHexFile = "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==";
                 // String rdbBinaryFile = "";
                 // for(int i = 0 ; i < rdbHexFile.length()  ; i++) {
                 //   rdbBinaryFile += Integer.toBinaryString(Integer.parseInt(rdbHexFile.charAt(i)+"", 16));
