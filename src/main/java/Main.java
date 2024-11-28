ddeab7639070cc7e46b73b83a99ef066add774ab
shomil03 <shomil02020@gmail.com>
2024-11-28 18:07:22 +0530

    bug fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -164,7 +164,7 @@ public class Main{
               break;  
 
           case "psync":
-                response = makeBulkString("+FULLRESYNC "+master_replicationID+" "+master_replicationOffset+"\r\n", false);
+                
                 
                 // String emptyRDBFile = "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==";
                 byte[] bytes = HexFormat.of().parseHex("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2");
@@ -177,8 +177,10 @@ public class Main{
                 // byte[] temp = response.getBytes();
                 
                 // System.out.println("byte array of empty hex file" +bytes.toString());
-                response = "$"+bytes.length+"\r\n";
+                
+                response = makeBulkString("+FULLRESYNC "+master_replicationID+" "+master_replicationOffset+"\r\n", false);
                 clientSocket.getOutputStream().write(response.getBytes());
+                response = "$"+bytes.length+"\r\n";
                 clientSocket.getOutputStream().write(response.getBytes());
                 clientSocket.getOutputStream().write(bytes);
                 response = null;
